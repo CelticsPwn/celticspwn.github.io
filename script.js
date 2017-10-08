a0df86979d2aac50652b56ceaf01bc488ef3e999
CelticsPwn <gmlawrencezhao@gmail.com>
2017-10-08 14:12:21 -0700

    version 1

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
 function openTab(tabName, elmnt, color) {
     // Hide all elements with class="tabcontent" by default */
     var i, tabcontent, tablinks;
